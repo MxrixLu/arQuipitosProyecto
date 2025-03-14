cc63a3f2c8f27cf2b87383fdf1353f1ff8c05b05
Maria Lu <m.benavidesd@uniandes.edu.co>
2025-03-13 22:45:22 -0500

    brainwave

--- brainwave/brainwave/urls.py
+++ brainwave/brainwave/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from brainwave.brainwave import views
+from brainwave import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
