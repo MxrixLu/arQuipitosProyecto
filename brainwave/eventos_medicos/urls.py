0b0aaaa61d8506ef0dd874de9249bb688e5359f1
Nikol Rodriguez <nk.rodriguez@uniandes.edu.co>
2025-03-30 15:02:11 -0500

    .

--- brainwave/eventos_medicos/urls.py
+++ brainwave/eventos_medicos/urls.py
@@ -1,25 +1,19 @@
-# eventos_medicos/urls.py
 from django.urls import path
 from . import views
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Bienvenido a Brainwave</h1>")
 
 urlpatterns = [
-    # URL para cargar imágenes de resonancia
     path('examen/<int:examen_id>/cargar-imagen/', views.cargar_imagen, name='cargar_imagen'),
-
-    # URL para ver imágenes de un examen
     path('examen/<int:examen_id>/ver-imagenes/', views.ver_imagenes, name='ver_imagenes'),
-    
     path('resonancias/', views.lista_imagenes_resonancias, name='lista_imagenes_resonancias'),
-    
     path('ver-resonancias/', views.vista_resonancias, name='vista_resonancias'),
-    
     path('generar-imagenes/', views.generar_imagenes, name='generar_imagenes'),
     path('eliminar-imagenes/', views.eliminar_imagenes, name='eliminar_imagenes'),
     path('generar-diagnosticos/', views.generar_diagnosticos_masivos, name='generar_diagnosticos'),
     path('listar-diagnosticos/', views.listar_diagnosticos, name='listar_diagnosticos'),
     path('eliminar-diagnosticos/', views.eliminar_diagnosticos, name='eliminar_diagnosticos'),
-
-
-
-    
+    path("", home, name="home"),  # Página de inicio
 ]
