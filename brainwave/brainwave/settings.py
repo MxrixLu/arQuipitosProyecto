53e1982000eecf9ed39344d236b6c239b00f16a9
Maria Lu <m.benavidesd@uniandes.edu.co>
2025-04-27 18:25:06 -0500

    settings

--- brainwave/brainwave/settings.py
+++ brainwave/brainwave/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'brainwave.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': 'brainwave_db',
+        'USER':'brainwave_user',
+        'PASSWORD': 'isis2503',
+        'HOST': '10.128.0.52',
+        'PORT':'5432',
     }
 }
 
