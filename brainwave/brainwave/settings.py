3786cd3c1d941efbe6b642b6da23bf7d1519b56f
Maria Lu <m.benavidesd@uniandes.edu.co>
2025-05-02 11:23:27 -0500

    Revert "settings"
    This reverts commit 53e1982000eecf9ed39344d236b6c239b00f16a9.

--- brainwave/brainwave/settings.py
+++ brainwave/brainwave/settings.py
@@ -94,11 +94,11 @@ WSGI_APPLICATION = 'brainwave.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_db_name',
-        'USER': 'your_db_user',
-        'PASSWORD': 'your_password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
