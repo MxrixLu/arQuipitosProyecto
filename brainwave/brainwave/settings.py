3e0f11110b26d22ce751e5a296e97077bbcc58ad
Maria Lu <m.benavidesd@uniandes.edu.co>
2025-05-04 20:04:57 -0500

    arreglo de settings

--- brainwave/brainwave/settings.py
+++ brainwave/brainwave/settings.py
@@ -98,13 +98,6 @@ DATABASES = {
     }
 }
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 
 
