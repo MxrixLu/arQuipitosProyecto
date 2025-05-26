a23f4246d0758c737bd319aa8e17e787c131f972
kmiloquimbay <d.quimbay@uniandes.edu.co>
2025-05-26 12:47:00 -0500

    .

--- eventos_ms/eventos/urls.py
+++ eventos_ms/eventos/urls.py
@@ -3,4 +3,16 @@ from .views import eventos
 
 urlpatterns = [
     url(r'^eventos/$', eventos),
+    url(r'^eventos/(?P<evento_id>[a-zA-Z0-9]+)/$', eventos),
+    url(r'^eventos/(?P<evento_id>[a-zA-Z0-9]+)/update/$', eventos),
+    url(r'^eventos/(?P<evento_id>[a-zA-Z0-9]+)/delete/$', eventos),
+    url(r'^pacientes/$', eventos),
+    url(r'^pacientes/(?P<paciente_id>[a-zA-Z0-9]+)/$', eventos),
+    url(r'^pacientes/(?P<paciente_id>[a-zA-Z0-9]+)/update/$', eventos),
+    url(r'^pacientes/(?P<paciente_id>[a-zA-Z0-9]+)/delete/$', eventos),
+    url(r'^doctores/$', eventos),
+    url(r'^doctores/(?P<doctor_id>[a-zA-Z0-9]+)/$', eventos),
+    url(r'^doctores/(?P<doctor_id>[a-zA-Z0-9]+)/update/$', eventos),
+    url(r'^doctores/(?P<doctor_id>[a-zA-Z0-9]+)/delete/$', eventos),
+    
 ]
