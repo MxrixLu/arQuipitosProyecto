bc4bc18ef8fbb9efa34188c5f2fa5e81cdd95e2b
kmiloquimbay <d.quimbay@uniandes.edu.co>
2025-05-26 12:31:55 -0500

    .

--- eventos_ms/eventos_ms/urls.py
+++ eventos_ms/eventos_ms/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('eventos/', include('eventos.urls')),
+    path('', include('eventos.urls')),
 ]
