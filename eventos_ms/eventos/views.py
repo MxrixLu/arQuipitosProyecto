e6e692fad7be82fbc4dfce118c76905f9aa6f810
kmiloquimbay <d.quimbay@uniandes.edu.co>
2025-05-26 12:35:05 -0500

    /

--- eventos_ms/eventos/views.py
+++ eventos_ms/eventos/views.py
@@ -1,3 +1,4 @@
+import eventos.logic as logic
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from django.http import JsonResponse
@@ -5,9 +6,8 @@ from django.http import JsonResponse
 @api_view(['GET', 'POST'])
 def eventos(request):
     if request.method == 'GET':
-        # Here you would typically retrieve data from the database
-        data = {"message": "This is a GET request"}
-        return JsonResponse(data, safe=False)
+        eventos = logic.get_eventos()
+        return JsonResponse([event.__dict__ for event in eventos], safe=False, status=200)
 
     elif request.method == 'POST':
         # Parse the incoming JSON data
