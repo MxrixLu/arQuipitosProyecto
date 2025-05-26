5afb514a0a5d2f690ced4a4773bf4eebc10f05fa
kmiloquimbay <d.quimbay@uniandes.edu.co>
2025-05-26 12:36:11 -0500

    .

--- eventos_ms/eventos/views.py
+++ eventos_ms/eventos/views.py
@@ -7,11 +7,5 @@ from django.http import JsonResponse
 def eventos(request):
     if request.method == 'GET':
         eventos = logic.get_eventos()
+        print(f"Retrieved {len(eventos)} eventos from the database.")
         return JsonResponse([event.__dict__ for event in eventos], safe=False, status=200)
-
-    elif request.method == 'POST':
-        # Parse the incoming JSON data
-        data = JSONParser().parse(request)
-        # Here you would typically save the data to the database
-        response_data = {"message": "Data received", "data": data}
-        return JsonResponse(response_data, status=201)
