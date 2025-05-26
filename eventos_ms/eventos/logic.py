2a6109771fd57470b445ca882a7b796da49e7094
kmiloquimbay <d.quimbay@uniandes.edu.co>
2025-05-26 12:38:02 -0500

    .

--- eventos_ms/eventos/logic.py
+++ eventos_ms/eventos/logic.py
@@ -6,9 +6,13 @@ import datetime
 
 
 def get_eventos():
+    print("Connecting to MongoDB...")
     client = MongoClient(settings.MONGO_CLI)
+    print("Connected to MongoDB.")
     db = client.eventos_db
+    print("Fetching eventos from the database...")
     eventos_collection = db['eventos']
+    print(f"Found {eventos_collection.count_documents({})} eventos in the collection.")
     eventos_collection = eventos_collection.find({})
     eventos = [Evento.from_mongo(doc) for doc in eventos_collection]
     client.close()
