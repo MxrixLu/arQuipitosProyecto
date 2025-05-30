b4ae587af65774649123ca6000f02bf8ff827780
Nikol Rodriguez <nk.rodriguez@uniandes.edu.co>
2025-05-30 16:29:49 -0500

    .

--- auth/auth_app/views.py
+++ auth/auth_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+def home(request):
+    return render(request, 'home.html')
 
-# Create your views here.
+@login_required
+def eventos_view(request):
+    return render(request, 'eventos.html')
+
+@login_required
+def analisis_view(request):
+    return render(request, 'analisis.html')
