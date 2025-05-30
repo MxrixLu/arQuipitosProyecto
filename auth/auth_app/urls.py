34f78a090dd2271c9e345177a0f389667ae6a7a9
Nikol Rodriguez <nk.rodriguez@uniandes.edu.co>
2025-05-30 16:33:12 -0500

    .

--- auth/auth_app/urls.py
+++ auth/auth_app/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('', include('eventos_ms.urls')),
     path(r'', include('django.contrib.auth.urls')),
     path(r'', include('social_django.urls')),
+     path('', views.home, name='home'),
+    path('eventos/', views.eventos_view, name='eventos'),
+    path('analisis/', views.analisis_view, name='analisis'),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('social_django.urls')),
 ]
