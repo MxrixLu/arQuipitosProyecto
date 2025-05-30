b8fd7b5344be119238a64e1e83a64deb87954cc1
Nikol Rodriguez <nk.rodriguez@uniandes.edu.co>
2025-05-30 16:12:53 -0500

    .

--- auth/manage.py
+++ auth/manage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+# Agrega la carpeta raíz del proyecto al PYTHONPATH para que Django encuentre todas las apps
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 def main():
     """Run administrative tasks."""
@@ -16,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
